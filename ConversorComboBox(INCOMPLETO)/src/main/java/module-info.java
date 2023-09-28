822172662851b6af96977f965013d10f71322ef4
João Pedro Scheffler <98850481+JP-Out@users.noreply.github.com>
2023-09-27 22:02:31 -0300

    Delete ConversorComboBox(INCOMPLETO) directory

--- ConversorComboBox(INCOMPLETO)/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.conversorcombobox {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.conversorcombobox to javafx.fxml;
-    exports com.mycompany.conversorcombobox;
-}
